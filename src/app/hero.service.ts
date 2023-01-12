1f8789d48adbd36ecbd1cc405f3a801a419c22bf
Javier Lete <jglete@eskura21.com>
2023-01-12 20:05:51 +0100

    Add services

---
+++ src/app/hero.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Hero } from './hero';
+import { MessageService } from './message.service';
+import { HEROES } from './mock-heroes';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = HEROES;
+    this.messageService.add('HeroService: fetched heroes');
+    return of(heroes);
+  }
+}
